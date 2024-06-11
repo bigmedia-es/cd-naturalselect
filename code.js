5e1f9d64de82c71ddc158cec7b3c41be89ec49c4
Big Media <141569796+bigmedia-es@users.noreply.github.com>
2024-06-11 22:13:51 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,27 @@
 jQuery(document).ready(function($) {
-    $('#proveedor_productos').select2();
+        $('#proveedor_productos').select2();
 
-    $('#proveedor_productos').on('change', function() {
-        var listaProductos = $('#lista-productos');
-        listaProductos.empty();
+        // Función para actualizar la lista de cantidades cuando se cambian los productos seleccionados
+        $('#proveedor_productos').on('change', function() {
+            var listaProductos = $('#lista-productos');
+            listaProductos.empty();
 
-        $(this).find('option:selected').each(function() {
-            var productoNombre = $(this).text();
-            var productoID = $(this).val();
+            $(this).find('option:selected').each(function() {
+                var productoID = $(this).val();
+                var productoNombre = $(this).text();
 
-            listaProductos.append('<li>' + productoNombre + ' <input type="number" name="cantidad_' + productoID + '" value="1" min="1"></li>');
+                // Obtener la cantidad actual del producto seleccionado
+                var cantidad = $('#cantidad_' + productoID).val();
+
+                // Si la cantidad no está definida, establecerla en 1 por defecto
+                if (!cantidad) {
+                    cantidad = 1;
+                }
+
+                listaProductos.append('<li>' + productoNombre + ' <input type="number" id="cantidad_' + productoID + '" name="cantidad_' + productoID + '" value="' + cantidad + '" min="1"></li>');
+            });
         });
+
+        // Función para actualizar la lista de cantidades al cargar la página
+        $('#proveedor_productos').trigger('change');
     });
-});
